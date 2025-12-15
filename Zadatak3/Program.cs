91ab7ab4858b11679345135b99a5f0db1d286bad
Luka <luka.orlic.ns@gmail.com>
2025-12-15 23:17:12 +0100

    Nesto

--- Zadatak3/Program.cs
+++ Zadatak3/Program.cs
@@ -11,7 +11,7 @@ namespace Zadatak3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            Console.WriteLine("Wassup");
+            Console.WriteLine("W");
         }
     }
 }
